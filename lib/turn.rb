0102e5a6e4f72a1ab9a2d222b2af70a5b512fdc5
Weston Schutt <wtschutt@gmail.com>
2023-05-02 11:07:46 -0600

    Add Winner method to Turn class

--- lib/turn.rb
+++ lib/turn.rb
@@ -16,4 +16,22 @@ class Turn
             :basic
         end
     end
+
+    def winner
+        if type == :basic
+            if player1.deck.cards[0].rank > player2.deck.cards[0].rank
+                player1
+            else
+                player2
+            end
+        elsif type == :war
+            if player1.deck.cards[2].rank > player2.deck.cards[2].rank
+                player1
+            else
+                player2
+            end
+        else # :mutually_assured_destruction
+            "No Winner"
+        end
+    end
 end
