384701ed3d1bc9d3749f18935a1c0a633da51931
Weston Schutt <wtschutt@gmail.com>
2023-05-05 18:10:30 -0600

    Add Turn class

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,17 +1,21 @@
 class Turn
-    attr_reader :player1, :player2, :spoils_of_war
+    attr_reader :player1, :player2, :spoils_of_war, :type
 
     def initialize(player1,player2)
         @player1 = player1
         @player2 = player2
         @spoils_of_war = []
+        @type = determine_type
     end
 
-    def type
-        if player1.deck.cards[0].rank == player2.deck.cards[0].rank && player1.deck.cards[2].rank == player2.deck.cards[2].rank
-            :mutually_assured_destruction
-        elsif player1.deck.cards[0].rank == player2.deck.cards[0].rank
-            :war
+    def determine_type
+        p1_card = player1.deck.cards
+        p2_card = player2.deck.cards
+        
+        if p1_card[0].rank == p2_card[0].rank && p1_card[2].rank == p2_card[2].rank
+            type = :mutually_assured_destruction
+        elsif p1_card[0].rank == p2_card[0].rank
+            type = :war
         else
             :basic
         end
@@ -37,15 +41,24 @@ class Turn
 
     def pile_cards
         if type == :basic
-            spoils_of_war << player1.deck.cards.shift
-            spoils_of_war << player2.deck.cards.shift
+            spoils_of_war << player1.deck.remove_card
+            spoils_of_war << player2.deck.remove_card
         elsif type == :war
-            spoils_of_war << player1.deck.cards.shift(3)
-            spoils_of_war << player2.deck.cards.shift(3)
-            spoils_of_war.flatten!
+            3.times do 
+                spoils_of_war << player1.deck.remove_card
+            end
+            3.times do
+                spoils_of_war << player2.deck.remove_card
+            end
         else # :mutually_assured_destruction
-            player1.deck.cards.shift(3)
-            player2.deck.cards.shift(3) 
+            3.times do player1.deck.remove_card end
+            3.times do player2.deck.remove_card end
         end 
     end
+
+    def award_spoils(winner)
+        until @spoils_of_war == []
+            winner.deck.add_card(spoils_of_war.shift)
+        end
+    end 
 end
